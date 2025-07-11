8da5d1ab07075ca88d2501ddd1a9c358dabda603
anuragkashyap302 <kumaranuragg68@gmail.com>
2025-07-11 12:45:22 +0530

    fix: correct typo in 'image' field of UserSchema

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const UserSchema = mongoose.Schema({
     _id :{type: String , required:true},
      username :{type: String , required:true},
     email :{type: String , required:true},
-     imgae :{type: String , required:true},
+     image :{type: String , required:true},
     role :{type: String , enum: ["user" , "hotelOwner"] , default:"user"},
      recentSearchCities :[{type: String , required:true}],
 } ,{timestamps:true}
